6b451c37ab0cb5eec94928c6e9a9d766f1399617
moorkunste <46429584+moorkunste@users.noreply.github.com>
2019-01-20 01:46:27 +1300

    fixed profile.class errors
    fixed some small detailes since the last push

--- Profile.java
+++ Profile.java
@@ -1,8 +1,8 @@
 import java.io.*;
 
-public class Save{
-	
-}
+//public class Save{
+//	
+//}
 
 
 public class Profile{
@@ -10,7 +10,7 @@ public class Profile{
 	public String nameOfProfile;
 	public int numOfSaves;
 	public String[] Saves;
-	public numberOfProfiles;
+	public int numberOfProfiles;
 //==========================variables==============================
 	public int achivements;
 	int width;//variable to store width of window to adjust the window size.
@@ -23,8 +23,8 @@ public class Profile{
 	public static void main(String args[]) {
 		System.out.printf("Hello World!\n");
 		Profile p = new Profile();
-		p.createProfile("tester");
-		//p.load("test");
+		//p.createProfile("bob");
+		p.load("test3");
 		System.out.printf(""+p.achivements);
 		p.achivements = 50;
 		p.save();
@@ -34,7 +34,7 @@ public class Profile{
 		System.out.printf("Hello World!\n");
 	}
 	public boolean createProfile(String Name){
-		this.save();
+		//this.save();
 		File f = new File( System.getProperty("user.dir")+fileSeparator+"saves"+fileSeparator+Name+fileSeparator);
 		if(f.exists()){
 			System.out.printf("Profile already exists");
@@ -52,10 +52,10 @@ public class Profile{
 				achivements=0;
 				int width=500;//variable to store width of window to adjust the window size.
 				int height=500;//variable to store height of window to adjust the window size.
-				boolean audio=true;//variable that defines whether or not sound will play.
+				//boolean audio=true;//variable that defines whether or not sound will play.
 //=========================================================================================
 				save();
-				File f = new File( System.getProperty("user.dir")+fileSeparator+"saves"+fileSeparator+nameOfProfile+fileSeparator+nameOfProfile+".prf");
+				f = new File( System.getProperty("user.dir")+fileSeparator+"saves"+fileSeparator+nameOfProfile+fileSeparator+nameOfProfile+".prf");
 				FileOutputStream outStream = null;
 				BufferedOutputStream bos = null;
 				try{
@@ -63,13 +63,14 @@ public class Profile{
 					bos = new BufferedOutputStream(outStream);
 					bos.write(numberOfProfiles);
 					for(int i=0;i<numberOfProfiles;i++){
-						bos.write(this.listOfProfiles()[i].getBytes());
+//						bos.write(this.listOfProfiles()[i].getBytes());
 						bos.write(0);
 					}
 					bos.close();
 				}
 				catch(Exception e) {
 					// if any I/O error occurs
+					System.out.printf("\ncreate exception\n");
 					e.printStackTrace();
 				}
 				finally{try{
@@ -82,40 +83,41 @@ public class Profile{
 		}
 		return false;
 	}
-	public String[] listOfProfiles(){
-		File f = new File( System.getProperty("user.dir")+fileSeparator+"saves"+fileSeparator+"p.p");
-		InputStream inStream = null;
-		BufferedInputStream bis = null;
-		try {
-			// open input stream test.txt for reading purpose.
-			inStream = new FileInputStream(f);
-			// input stream is converted to buffered input stream
-			bis = new BufferedInputStream(inStream);			
-			// read until a single byte is available
-			numberOfProfiles=bis.read();
-			String[numberOfProfiles] parray = new String[numberOfProfiles];
-			for(int i=0;i<numberOfProfiles;i++){
-				int j=0;
-				char c=bis.read();
-				while(c!=0){
-					parray[i][j]=c;
-					c=bis.read();
-					j++;
-				}
-			}
-			return parray;
-		}
-		catch(Exception e) {
-			// if any I/O error occurs
-			e.printStackTrace();
-		}
-		finally {try{
-			// releases any system resources associated with the stream
-			if(inStream!=null) inStream.close();
-			if(bis!=null) bis.close();
-			}catch(Exception e){}
-		}
-	}
+	//public String[] listOfProfiles(){
+	//	File f = new File( System.getProperty("user.dir")+fileSeparator+"saves"+fileSeparator+"p.p");
+	//	InputStream inStream = null;
+	//	BufferedInputStream bis = null;
+	//	try {
+	//		// open input stream test.txt for reading purpose.
+	//		inStream = new FileInputStream(f);
+	//		// input stream is converted to buffered input stream
+	//		bis = new BufferedInputStream(inStream);			
+	//		// read until a single byte is available
+	//		numberOfProfiles=bis.read();
+	//		String[] parray = new String[numberOfProfiles];
+	//		for(int i=0;i<numberOfProfiles;i++){
+	//			int j=0;
+	//			char c= (char)bis.read();
+	//			while(c!=0){
+	//				parray[i][j]=c;
+	//				c=bis.read();
+	//				j++;
+	//			}
+	//		}
+	//		return parray;
+	//	}
+	//	catch(Exception e) {
+	//		// if any I/O error occurs
+	//		System.out.printf("\nlistOfProfiles exception\n");
+	//		e.printStackTrace();
+	//	}
+	//	finally {try{
+	//		// releases any system resources associated with the stream
+	//		if(inStream!=null) inStream.close();
+	//		if(bis!=null) bis.close();
+	//		}catch(Exception e){}
+	//	}
+	//}
 	public void load(String Name){
 		nameOfProfile = Name;
 		File f = new File( System.getProperty("user.dir")+fileSeparator+"saves"+fileSeparator+Name+fileSeparator+Name+".prf");
@@ -139,12 +141,13 @@ public class Profile{
 //===========================load values=======================================
 			achivements = (int)bis.read();
 			width = (int)bis.read();
-			hight = (int)bis.read();
-			audio = (boolean)bis.read();
+			height = (int)bis.read();
+			//audio = (boolean)bis.read();
 //==================================================================			
 		}
 		catch(Exception e) {
 			// if any I/O error occurs
+			System.out.printf("\nload exception\n");
 			e.printStackTrace();
 		}
 		finally {try{
@@ -168,13 +171,14 @@ public class Profile{
 //=================================save values====================================
 			bos.write(achivements);
 			bos.write(width);
-			bos.write(hight);
-			bos.write(audio);
+			bos.write(height);
+			//bos.write(audio);
 //=====================================================================
 			bos.close();
 		}
 		catch(Exception e) {
 			// if any I/O error occurs
+			System.out.printf("\nsave exception\n");
 			e.printStackTrace();
 		}
 		finally{try{
